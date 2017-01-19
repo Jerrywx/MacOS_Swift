b52b5499398d9514c8aebaab6244b9309b1d6e47
Jerrywx <23601145@qq.com>
2017-01-19 11:07:27 +0800

    添加 Guide项目

---
+++ Chapter 01 Guide/Swift_Guide/Swift_Guide/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Swift_Guide
+//
+//  Created by 王潇 on 2017/1/19.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
