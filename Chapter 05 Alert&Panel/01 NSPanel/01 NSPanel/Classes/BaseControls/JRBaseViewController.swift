9c19d1f5f3956e3236217ec4fa7d20054df3742c
Jerrywx <23601145@qq.com>
2017-01-22 16:40:35 +0800

    添加按钮

--- Chapter 05 Alert&Panel/01 NSPanel/01 NSPanel/Classes/BaseControls/JRBaseViewController.swift
+++ Chapter 05 Alert&Panel/01 NSPanel/01 NSPanel/Classes/BaseControls/JRBaseViewController.swift
@@ -40,7 +40,7 @@ extension JRBaseViewController {
 	fileprivate func setupUI() {
 		
 		/// 打开 普通 Pannel
-		let button			= NSButton(frame: CGRect(x: 100, y: 30, width: 120, height: 30))
+		let button			= NSButton(frame: CGRect(x: 100, y: 20, width: 120, height: 30))
 		button.bezelStyle	= .roundRect
 		button.title		= "打开 普通 Pannel"
 		button.target		= self
@@ -48,12 +48,60 @@ extension JRBaseViewController {
 		self.view.addSubview(button)
 		
 		/// 打开文件
-		let button2			= NSButton(frame: CGRect(x: 100, y: 90, width: 120, height: 30))
+		let button2			= NSButton(frame: CGRect(x: 100, y: 70, width: 120, height: 30))
 		button2.bezelStyle	= .roundRect
 		button2.title		= "打开 文件 Pannel"
-		button.target		= self
+		button2.target		= self
 		button2.action		= #selector(openFile)
 		self.view.addSubview(button2)
+		
+		/// 打开文件
+		let button3			= NSButton(frame: CGRect(x: 100, y: 120, width: 120, height: 30))
+		button3.bezelStyle	= .roundRect
+		button3.title		= "获取字符串"
+		button3.target		= self
+		button3.action		= #selector(openFile)
+		self.view.addSubview(button3)
+		
+		/// 打开文件
+		let button4			= NSButton(frame: CGRect(x: 100, y: 170, width: 120, height: 30))
+		button4.bezelStyle	= .roundRect
+		button4.title		= "获取图片"
+		button4.target		= self
+		button4.action		= #selector(openFile)
+		self.view.addSubview(button4)
+		
+		/// 打开文件
+		let button5			= NSButton(frame: CGRect(x: 100, y: 220, width: 120, height: 30))
+		button5.bezelStyle	= .roundRect
+		button5.title		= "保存字符串"
+		button5.target		= self
+		button5.action		= #selector(openFile)
+		self.view.addSubview(button5)
+		
+		/// 打开文件
+		let button8			= NSButton(frame: CGRect(x: 100, y: 270, width: 120, height: 30))
+		button8.bezelStyle	= .roundRect
+		button8.title		= "保存图片"
+		button8.target		= self
+		button8.action		= #selector(openFile)
+		self.view.addSubview(button8)
+		
+		/// 打开文件
+		let button6			= NSButton(frame: CGRect(x: 100, y: 320, width: 120, height: 30))
+		button6.bezelStyle	= .roundRect
+		button6.title		= "选择颜色"
+		button6.target		= self
+		button6.action		= #selector(openFile)
+		self.view.addSubview(button6)
+		
+		/// 打开文件
+		let button7			= NSButton(frame: CGRect(x: 100, y: 370, width: 120, height: 30))
+		button7.bezelStyle	= .roundRect
+		button7.title		= "选择字体"
+		button7.target		= self
+		button7.action		= #selector(openFile)
+		self.view.addSubview(button7)
 	}
 	////////////////////////////////////////////////////////
 	@objc private func openFile() {
