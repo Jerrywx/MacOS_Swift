850910133ae3f28bdebce734da6281db353261a9
Jerrywx <23601145@qq.com>
2017-02-06 23:48:23 +0800

    添加 TableView 项目

---
+++ Chapter 07 TableView&TreeView/TableView/TableView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TableView
+//
+//  Created by 王潇 on 2017/2/6.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
