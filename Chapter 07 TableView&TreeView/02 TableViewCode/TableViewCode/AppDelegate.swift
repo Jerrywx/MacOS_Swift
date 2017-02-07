525ab88718a791e40f862d65d916f14af7d27a5e
Jerrywx <23601145@qq.com>
2017-02-07 10:11:26 +0800

    添加代码NSTableView示例

---
+++ Chapter 07 TableView&TreeView/02 TableViewCode/TableViewCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableViewCode
+//
+//  Created by 王潇 on 2017/2/7.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Chapter 07 TableView&TreeView/02 TableViewCode/TableViewCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewCode
+//
+//  Created by 王潇 on 2017/2/7.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
